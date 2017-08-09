fd618748b6a9ed17a71ae0ff30d6f0abec328fe5
Unknown <miguelmalagon@outlook.com>
2017-08-09 16:28:39 -0700

    Add register section, the site works

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,4 +6,9 @@ class MainController < ApplicationController
   def unregistered
 
   end
+  protected
+    def set_layout
+      return  "landing" if action_name == "unregistered"
+      super
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def resource
+    # el operador || asigna el valor del lado derecho a la variable del lado izquierdo siempre y cuando la variable izquierda
+    # este nula si no lo esta no asigna el valor
+    @resource ||=User.new
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource_classs
+    User
+  end
 end
